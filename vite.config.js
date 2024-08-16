b99a2ce6282769ae7bec2b42c905babf4c691a00
DanielDCSS <danielcarlosss12@icloud.com>
2024-08-16 11:27:22 -0300

    agr vai

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //base: "/ModelosReact", //ATENÇÃO: Em ambiente de desenvolvimento comentar essa linha. Descomentar apenas em deploy
+  base: "/ModelosReact", //ATENÇÃO: Em ambiente de desenvolvimento comentar essa linha. Descomentar apenas em deploy
 
   publicDir: 'public',
   build: {
